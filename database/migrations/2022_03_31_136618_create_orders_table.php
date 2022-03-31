acaa00f151025cd14cdd45c206ae821413fb7403
WongKK <elviswong2002@hotmail.com>
2022-04-01 01:12:04 +0800

    Create foreign key linkage & Include bootstrap cdn.

---
+++ database/migrations/2022_03_31_136618_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('delivery_time');
+            $table->string('total_price');
+            $table->unsignedBigInteger('payment_id');
+            $table->boolean('is_prepared')->default(false);
+            $table->boolean('is_delivered')->default(false);
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
