3c4f26e9cf8e550578f48574d573e9fbee92e9f1
WongKK <elviswong2002@hotmail.com>
2022-04-05 16:11:04 +0800

    Fix Bug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
@@ -23,10 +24,10 @@ class Controller extends BaseController
     public function index() {
 
         if(Auth::user()->is_admin){
-            return view("home");
+            return view("admin");
         }
         else{
-            return view("admin");
+            return view("home");
         }
     }
 }
