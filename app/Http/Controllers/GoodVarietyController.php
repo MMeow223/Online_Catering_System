451eb5d7914c3d38173ae628dce86e5307701be8
WongKK <elviswong2002@hotmail.com>
2022-04-05 16:45:35 +0800

    Add Documentation

--- app/Http/Controllers/GoodVarietyController.php
+++ app/Http/Controllers/GoodVarietyController.php
@@ -38,9 +38,6 @@ class GoodVarietyController extends Controller
      */
     public function store(Request $request)
     {
-
-
-//        dd("store");
         //
     }
 
@@ -88,11 +85,18 @@ class GoodVarietyController extends Controller
     {
         //
     }
+
+    /**
+     * @param Request $request
+     * @return void
+     */
     public static function staticStoreGoodVariety(Request $request){
-//        dd($request);
-        if($request->input('new_good_variety')!=null){
+        // if there is new variety entered
+        if($request->input('new_good_variety') != null){
+            // separate the varieties by comma
             $good_varieties = explode(',',$request->input('new_good_variety'));
 
+            // loop through the varieties and create new varieties
             foreach ($good_varieties as $good_variety) {
                 GoodVariety::create([
                     'good_id' => Good::where('good_name',$request->input('name'))->first()->id,
@@ -103,39 +107,36 @@ class GoodVarietyController extends Controller
         }
     }
 
+    /**
+     * @param Request $request
+     * @return void
+     */
     public static function staticEditGoodVariety(Request $request){
 
+        // get all the keys from the request list
         $keys = array_keys($request->all());
+
+        // get the keys that contain "variety-checkbox-"
         $keys = array_filter($keys, function($key) {
             return str_contains($key, 'variety-checkbox-');
         });
 
+        // get the id from these keys
         $keys = array_map(function($key) {
             return intval(str_replace('variety-checkbox-', '', $key));
         }, $keys);
 
-        $available_varieties_id = array_values($keys);
+        // get all the varieties that are belongs to the good
         $good_varieties = GoodVariety::where('good_id',Good::where('good_name',$request->input('name'))->first()->id)->get();
 
+        // loop through the varieties and add them to an array
         $available_varieties_id = array();
-//        dd($good_varieties);
         foreach ($good_varieties as $good_variety){
             // insert it into an array
             array_push($available_varieties_id,$good_variety->id);
         }
 
-//        dd($available_varieties_id);
-
-//        dd($good_varieties);
-//        foreach ($good_varieties as $good_variety){
-//            GoodVariety::find($good_variety->id)->update([
-//                'is_available' => 0
-//            ]);
-//            $good_variety->save();
-//        }
-//        dd($available_varieties_id);
-
-
+        // loop and check if the requested varieties match existing varieties, if so, update them to available, otherwise, make them unavailable
         foreach ($available_varieties_id as $available_variety_id){
             if(in_array($available_variety_id,$keys)){
                 GoodVariety::find($available_variety_id)->update([
@@ -147,29 +148,5 @@ class GoodVarietyController extends Controller
                 ]);
             }
         }
-//        for($i=0;$i<count($good_varieties);$i++){
-//            if(in_array($good_varieties[$i]->id,$available_varieties_id)){
-//                GoodVariety::find($good_varieties[$i]->id)->update([
-//                    'is_available' => 1
-//                ]);
-//            }else{
-//                GoodVariety::find($good_varieties[$i]->id)->update([
-//                    'is_available' => 0
-//                ]);
-//            }
-//        }
-//        foreach ($good_varieties as $good_variety){
-//            if(in_array($good_variety->id,$available_varieties_id)){
-//                GoodVariety::find($good_variety->id)->update([
-//                    'is_available' => 1
-//                ]);
-//            }else{
-//                GoodVariety::find($good_variety->id)->update([
-//                    'is_available' => 0
-//                ]);
-//            }
-//            $good_variety->save();
-//        }
     }
-
 }
