01472ba306b8523111f8e707c78224ecc7b70b93
tonny767 <tonny.wijayajuly2001@gmail.com>
2022-04-06 20:35:01 +0800

    fix Goods Views page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,6 +107,12 @@
                                             <span>Dashboard</span>
                                         </a>
                                     </li>
+                                    <li>
+                                        <a href="{{route('goods.index')}}" class="nav-link px-3 active">
+                                            <span class="me-2"><i class="bi bi-speedometer2"></i></span>
+                                            <span>Goods Manage</span>
+                                        </a>
+                                    </li>
                                 </ul>
                             </nav>
                         </div>
