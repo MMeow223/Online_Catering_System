112666480c14c2358d33059735cf0ea384f8a8e1
WongKK <elviswong2002@hotmail.com>
2022-04-08 20:42:31 +0800

    Fix Content Missing For Guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,9 +66,10 @@
                         @endif
                     @else
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button"
+                            <a id="navbarDropdown" class="text-decoration-none dropdown-toggle text-white" href="#" role="button"
                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->username }}
+                                <input class="rounded text-white" type="text" value="{{ Auth::user()->username }}" disabled/>
+{{--                                {{ Auth::user()->username }}--}}
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
@@ -90,14 +91,16 @@
     </nav>
 
     <main class="py-4">
+        @guest
+            @include('inc.message')
+            @yield('content')
+        @else
 
-    @guest
+            <div class="row m-auto p-auto">
+                @if(auth()->user()->is_admin)
 
-    @else
-        <div class="row m-auto p-auto">
-            <div class="col-3">
-            @if(auth()->user()->is_admin)
-                <!--Admin Dashboard-->
+                <div class="col-3">
+                    <!--Admin Dashboard-->
                     <div
                         class="offcanvas offcanvas-start sidebar-nav bg-dark"
                         tabindex="-1"
@@ -112,8 +115,8 @@
                                             <hr>
                                         </div>
                                     </li>
-                                    <li class="m-2  {{ (request()->is('home*')) ? 'bg-opacity-50 bg-secondary rounded' : '' }}">
-                                        <a href="{{route('home')}}" class="nav-link px-3 active">
+                                    <li class="mx-2  {{ (request()->is('/')) ? 'bg-opacity-50 bg-secondary rounded' : '' }}">
+                                        <a href="/" class="nav-link px-3 {{ (request()->is('/')) ? 'active' : '' }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-window" viewBox="0 0 16 16">
                                                 <path d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
                                                 <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm13 2v2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zM2 14a1 1 0 0 1-1-1V6h14v7a1 1 0 0 1-1 1H2z"/>
@@ -121,26 +124,43 @@
                                             <span>Dashboard</span>
                                         </a>
                                     </li>
-                                    <li class="m-2 {{ (request()->is('goods*')) ? 'bg-opacity-50 bg-secondary rounded' : '' }}">
-                                        <a href="{{route('goods.index')}}" class="nav-link px-3 active">
+                                    <li class="mx-2 {{ (request()->is('goods*')) ? 'bg-opacity-50 bg-secondary rounded' : '' }}">
+                                        <a href="{{route('goods.index')}}" class="nav-link px-3 {{ (request()->is('goods*')) ? 'active' : '' }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box" viewBox="0 0 16 16">
                                                 <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
                                             </svg>
                                             <span>Goods Manage</span>
                                         </a>
                                     </li>
+                                    <li class="mx-2 {{ (request()->is('orders*')) ? 'bg-opacity-50 bg-secondary rounded' : '' }}">
+                                        <a href="/orders" class="nav-link px-3 {{ (request()->is('orders*')) ? 'active' : '' }}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-receipt" viewBox="0 0 16 16">
+                                                <path d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z"/>
+                                                <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
+                                            </svg>
+                                            <span>Order Manage</span>
+                                        </a>
+                                    </li>
+                                    <li class="mx-2 {{ (request()->is('accounts*')) ? 'bg-opacity-50 bg-secondary rounded' : '' }}">
+                                        <a href="/accounts" class="nav-link px-3 {{ (request()->is('accounts*')) ? 'active' : '' }}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                                            </svg>
+                                            <span>Account Manage</span>
+                                        </a>
+                                    </li>
                                 </ul>
                             </nav>
                         </div>
                     </div>
-                @endif
-            </div>
-            <div class="col-8">
-                @include('inc.message')
-                @yield('content')
-            </div>
+                    @endif
+                </div>
+                <div class="col-8">
+                    @include('inc.message')
+                    @yield('content')
+                </div>
+            @endguest
         </div>
-    @endguest
     </main>
 </div>
 </body>
