68e845bcec0ce589a001c5a82f16e0870c61734f
tonny767 <tonny.wijayajuly2001@gmail.com>
2022-04-12 15:24:44 +0800

    Account Manage

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.app')
+
+@section('content')
+{{--    <div class="container">--}}
+        <div class="row justify-content-center">
+{{--            <div class="col-md-8">--}}
+                <div class="card shadow">
+                    <div class="card-header">
+                        <div class=" d-flex justify-content-between">
+                            <div>
+                                <h3 class="d-inline">{{ __('User') }}</h3>
+                                <small class="text-muted d-inline"> (View all goods name, price, and description)</small>
+                            </div>
+                            <div>
+                                <a href="{{ route('users.create') }}"
+                                   class="btn btn-primary float-right">{{ __('Create') }}</a>
+                                <a href="{{ url()->previous() }}"
+                                   class="btn btn-secondary float-right">{{ __('Back') }}</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>{{ __('Username') }}</th>
+                                <th>{{ __('Email') }}</th>
+                                <th>{{ __('Password') }}</th>
+                                <th>{{ __('Owner ID') }}</th>
+                                <th>{{ __('Last Updated') }}</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $user->username }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>********</td>
+                                    <td>{{ $user->owner_id }}</td>
+                                    <td>{{ $user->updated_at}}</td>
+                                    <td>
+                                        <a href="{{ route('users.show', $user->id) }}"
+                                           class="btn btn-secondary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-lg" viewBox="0 0 16 16">
+                                                <path d="m9.708 6.075-3.024.379-.108.502.595.108c.387.093.464.232.38.619l-.975 4.577c-.255 1.183.14 1.74 1.067 1.74.72 0 1.554-.332 1.933-.789l.116-.549c-.263.232-.65.325-.905.325-.363 0-.494-.255-.402-.704l1.323-6.208Zm.091-2.755a1.32 1.32 0 1 1-2.64 0 1.32 1.32 0 0 1 2.64 0Z"/>
+                                            </svg>
+                                        </a>
+                                        <a href="{{ route('users.edit', $user->id) }}"
+                                           class="btn btn-primary">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                            </svg>
+                                        </a>
+                                        <form action="{{ route('users.destroy', $user->id) }}" method="POST"
+                                              class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                                                </svg>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="px-5">
+                        {{ $users->links('pagination::bootstrap-5') }}
+                    </div>
+                </div>
+            </div>
+{{--        </div>--}}
+
+{{--    </div>--}}
+@endsection
