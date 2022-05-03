42ccfff3a3011f3cd82acee931f95234613361d5
WongKK <elviswong2002@hotmail.com>
2022-05-04 00:41:57 +0800

    Add routes for functions used for AJAX and

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,12 @@ Route::resource('orders', \App\Http\Controllers\OrderController::class);
 Route::resource('users', \App\Http\Controllers\UsersController::class);
 Route::resource('cart', \App\Http\Controllers\ShoppingCartController::class);
 
+Route::get('/cart/update/select/{item_id}',[\App\Http\Controllers\ShoppingCartController::class, 'updateSelected'])->name('cart.select');
+Route::get('/cart/update/quantity/increase/{item_id}',[\App\Http\Controllers\ShoppingCartController::class, 'updateIncreaseQuantity']);
+Route::get('/cart/update/quantity/decrease/{item_id}',[\App\Http\Controllers\ShoppingCartController::class, 'updateDecreaseQuantity']);
+Route::get('/cart/update/cartItemPrice/{item_id}',[\App\Http\Controllers\ShoppingCartController::class, 'calculateCartItemPrice']);
+
+
 Route::get('/filterCategory/{category_id}', [\App\Http\Controllers\Controller::class, 'filterGoodBasedOnCategory'])->name('filterCategory');
 
 
