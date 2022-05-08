6bc06b0634dc01498dee328bef2b688151d9aab8
WongKK <elviswong2002@hotmail.com>
2022-05-09 02:52:01 +0800

    Add new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PromotionVoucher;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,6 +23,7 @@ class DatabaseSeeder extends Seeder
             GoodsVarietySeeder::class,
             OrdersSeeder::class,
             PaymentSeeder::class,
+            PromotionVoucherSeeder::class,
         ]);
     }
 }
