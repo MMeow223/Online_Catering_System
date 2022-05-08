e4893d2c70f8b52f67afce323bd39e1b9a34e17e
WongKK <elviswong2002@hotmail.com>
2022-05-09 02:52:23 +0800

    Update ShoppingCartController.php

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\CheckoutGoods;
 use App\Models\Good;
 use App\Models\GoodVariety;
+use App\Models\PromotionVoucher;
 use App\Models\ShoppingCart;
 use App\Http\Requests\StoreShoppingCartRequest;
 use App\Http\Requests\UpdateShoppingCartRequest;
 use App\Models\User;
+use App\Models\UserVoucher;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
@@ -28,7 +30,19 @@ class ShoppingCartController extends Controller
             ->select('shopping_carts.*', 'goods.id as goods_id', 'goods.good_image','goods.good_name','goods.good_price', 'good_varieties.variety_name as good_variety_name', 'users.username as username')
             ->get();
 
+        $vouchers = UserVoucher::join('promotion_vouchers', 'user_vouchers.voucher_code', '=', 'promotion_vouchers.voucher_code')
+            ->where('user_id', Auth::id())
+            ->select('user_vouchers.*', 'promotion_vouchers.voucher_name as voucher_name', 'promotion_vouchers.discount as discount','promotion_vouchers.price_limit as price_limit')
+            ->get();
+
+        $current_using_voucher = UserVoucher::join('promotion_vouchers', 'user_vouchers.voucher_code', '=', 'promotion_vouchers.voucher_code')
+            ->where('user_id', Auth::id())
+            ->where('use_status', 'using')
+            ->select('user_vouchers.*', 'promotion_vouchers.voucher_name as voucher_name', 'promotion_vouchers.discount as discount','promotion_vouchers.price_limit as price_limit')
+            ->first();
+
         $total_price = $this->calculateTotalPrice();
+        $total_price_after_discount = $this->calculateTotalPriceWithDiscount($current_using_voucher);
 
         $selectedItemCount = 0;
         foreach ($cart_items as $cart_item) {
@@ -41,7 +55,21 @@ class ShoppingCartController extends Controller
         return view('cart.index')
             ->with('cart_items', $cart_items)
             ->with('total_price', $total_price)
-            ->with('selectedItemCount', $selectedItemCount);
+            ->with('total_price_after_discount', $total_price_after_discount)
+            ->with('selectedItemCount', $selectedItemCount)
+            ->with('vouchers',$vouchers)
+            ->with('current_using_voucher',$current_using_voucher)
+            ->with('products',Good::paginate(40))
+            ;
+    }
+    public function resetVoucher($user_id){
+        $user_vouchers = UserVoucher::where('user_id', $user_id)->get();
+        foreach ($user_vouchers as $user_voucher){
+            $user_voucher->use_status = 'unused';
+            $user_voucher->save();
+        }
+
+        return redirect()->back();
     }
 
     public function updateSelected($item_id)
@@ -57,7 +85,8 @@ class ShoppingCartController extends Controller
         $cart_item = ShoppingCart::find($item_id);
         $cart_item->quantity += 1;
         $cart_item->save();
-        return $this->calculateTotalPrice();
+
+        return [$this->calculateTotalPrice(),$this->calculateTotalPriceWithDiscount(null)];
     }
 
     //TODO if the quantity is 0, delete the item from the cart
@@ -75,10 +104,32 @@ class ShoppingCartController extends Controller
         {
             $cart_item->delete();
         }
-        return $this->calculateTotalPrice();
+        return [$this->calculateTotalPrice(),$this->calculateTotalPriceWithDiscount(null)];
 
     }
 
+    public function updateSelectedVoucher($voucher_code)
+    {
+
+        $reset_voucher = UserVoucher::where('user_id', auth()->user()->id)
+        ->where('use_status', '=', 'using')
+        ->get();
+
+        foreach ($reset_voucher as $reset_voucher_item) {
+            $reset_voucher_item->use_status = 'unused';
+            $reset_voucher_item->save();
+        }
+
+        $voucher = UserVoucher::where('user_id', auth()->user()->id)
+            ->where('voucher_code', $voucher_code)
+            ->first();
+
+        $voucher->use_status = 'using';
+        $voucher->save();
+
+        return redirect()->route('cart.index');
+
+    }
     public function calculateCartItemPrice($item_id)
     {
         $cart_item = ShoppingCart::join('goods', 'shopping_carts.good_id', '=', 'goods.id')
@@ -105,6 +156,44 @@ class ShoppingCartController extends Controller
                 $total_price += $cart_item->good_price * $cart_item->quantity;
             }
         }
+
+        return $total_price;
+    }
+
+    public function calculateTotalPriceWithDiscount($current_using_voucher){
+
+        if($current_using_voucher == null) {
+            $current_using_voucher = UserVoucher::join('promotion_vouchers', 'user_vouchers.voucher_code', '=', 'promotion_vouchers.voucher_code')
+                ->where('user_id', Auth::id())
+                ->where('use_status', 'using')
+                ->select('user_vouchers.*', 'promotion_vouchers.voucher_name as voucher_name', 'promotion_vouchers.discount as discount','promotion_vouchers.price_limit as price_limit')
+                ->first();
+        }
+
+        $cart_items = ShoppingCart::join('goods', 'shopping_carts.good_id', '=', 'goods.id')
+            ->join('users', 'shopping_carts.user_id', '=', 'users.id')
+            ->join('good_varieties', 'shopping_carts.variation_id', '=', 'good_varieties.id')
+            ->select('shopping_carts.*', 'goods.good_image','goods.good_name','goods.good_price', 'good_varieties.variety_name as good_variety_name', 'users.username as username')
+            ->get();
+
+        $total_price = 0;
+        foreach ($cart_items as $cart_item) {
+            if ($cart_item->selected) {
+                $total_price += $cart_item->good_price * $cart_item->quantity;
+            }
+        }
+
+        if($current_using_voucher != null) {
+
+            $discount_amount = (($total_price/100) * $current_using_voucher->discount);
+
+            if($discount_amount > $current_using_voucher->price_limit){
+                $discount_amount = $current_using_voucher->price_limit;
+            }
+
+            $total_price -= $discount_amount;
+        }
+
         return $total_price;
     }
 
